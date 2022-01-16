3a347768145d3b480e851f102cd0279d9f3e8374
Splashyface <90992622+Splashyface@users.noreply.github.com>
2022-01-16 21:20:08 +0100

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace CV_Webbutveckling.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
---
+++ Models/Edgucation.cs
@@ -0,0 +1,11 @@
+﻿namespace CV_Webbutveckling.Models
+{
+    public class Edgucation
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
